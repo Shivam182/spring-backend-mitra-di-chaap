5acb00ec3c5533ff2c026299da4cd438252e4e26
Shivam Yadav <yadavshivam24012002@gmail.com>
2023-04-01 15:41:20 +0530

    jwt auth

--- src/main/java/com/api/mitra_di_chaap/MitraDiChaapApplication.java
+++ src/main/java/com/api/mitra_di_chaap/MitraDiChaapApplication.java
@@ -1,13 +1,26 @@
 package com.api.mitra_di_chaap;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.api.mitra_di_chaap.config.AppConstants;
+import com.api.mitra_di_chaap.entities.Role;
+import com.api.mitra_di_chaap.repositories.RoleRepo;
 
 @SpringBootApplication
 public class MitraDiChaapApplication {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private RoleRepo roleRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MitraDiChaapApplication.class, args);
@@ -17,6 +30,36 @@ public class MitraDiChaapApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	
+
+	
+//	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println(this.passwordEncoder.encode("passw"));
+		
+		try {
+			Role role1 = new Role();
+			role1.setId(AppConstants.ADMIN_USER);
+			role1.setName("ADMIN_USER");
+			
+			Role role2 = new Role();
+			role2.setId(AppConstants.NORMAL_USER);
+			role2.setName("ADMIN_USER");
+			
+			List<Role> roles = List.of(role1,role2);
+			List<Role> result = this.roleRepo.saveAll(roles);
+			
+			
+			result.forEach(r->{
+				System.out.println(r.getName());
+			});
+		}catch(Exception e) {
+			
+		}
+		
 	}
 	
 //	@Bean(name="entityManagerFactory")
